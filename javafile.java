c2036d6d16a0b8fcff1846503c35360802685018
ankit-r-r <ankitrupchandani@gmail.com>
2026-07-02 19:22:28 +0530

    Fix missing semicolon in javafile.java

--- javafile.java
+++ javafile.java
@@ -4,6 +4,6 @@ class Simple {
     public static void main(String[] args) {
         System.out.println("Hello, All this code is written to study jenkins!"); 
         System.out.println("Let start building CI/CD pipelines");
-        System.out.println("This line is written to check the CI")
+        System.out.println("This line is written to check the CI");
     }
 }
