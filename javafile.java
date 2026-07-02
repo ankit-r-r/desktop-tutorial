ad0fd55459a46201d6db8a54ca77437faa890cac
ankit-r-r <ankitrupchandani@gmail.com>
2026-07-02 19:19:26 +0530

    Fix typo in CI/CD pipelines message

--- javafile.java
+++ javafile.java
@@ -3,6 +3,6 @@
 class Simple {
     public static void main(String[] args) {
         System.out.println("Hello, All this code is written to study jenkins!"); 
-        System.out.println("Let start building CI/CD pipelines")
+        System.out.println("Let start building CI/CD pipelines");
     }
 }
